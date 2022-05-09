e103e6d25be4445ad57f3e43eecd516afbb48b78
Naoto Tsukamoto <naototukka0413@gmail.com>
2022-05-09 17:11:01 +0900

    [jsk_unitree_startup] Show weather and single comment

--- jsk_unitree_robot/jsk_unitree_startup/scripts/launch-email.py
+++ jsk_unitree_robot/jsk_unitree_startup/scripts/launch-email.py
@@ -3,6 +3,8 @@
 
 from bs4 import BeautifulSoup
 import datetime
+import json
+import random
 import rospy
 import subprocess
 import sys
@@ -16,6 +18,16 @@ class RobotLaunchEmail:
     """
     Send email when robot launched with tips.
     """
+    def __init__(self):
+        # Character code differences between python versions
+        # See https://stackoverflow.com/questions/54153986/handling-encode-when-converting-from-python2-to-python3 #NOQA
+        if sys.version_info.major == 2:
+            reload(sys)
+            sys.setdefaultencoding('utf-8')
+        api_key_file = '/var/lib/robot/openweathermap_api_key.txt'
+        with open(api_key_file, 'r') as f:
+            self.appid = f.read().split('\n')[0]
+
     def get_tips(self):
         """
         Get tips from zatsuneta random article.
@@ -43,6 +55,47 @@ class RobotLaunchEmail:
 
         return title, contents, detail_url
 
+    # Partly copied from https://github.com/knorth55/jsk_robot/blob/b2999b0cece82d7b1d46320a1e7c84e4fc078bd2/jsk_fetch_robot/jsk_fetch_startup/scripts/time_signal.py #NOQA
+    def get_weather_forecast(self, lang='en'):
+        url = 'http://api.openweathermap.org/data/2.5/weather?q=tokyo&lang={}&units=metric&appid={}'.format(lang, self.appid)  # NOQA
+        if sys.version_info.major == 2:
+            resp = json.loads(urllib2.urlopen(url).read())
+        else:
+            resp = json.loads(request.urlopen(url).read())
+        weather = resp['weather'][0]['description']
+
+        forecast_text = ""
+        if lang == 'ja':
+            forecast_text = "今日の天気は" + weather + "です。"
+        else:
+            forecast_text = " The weather is " + weather + " now."
+
+        # It feels like the robot is expressing its will based on the weather.
+        if "晴" in weather:
+            forecast_tuple = (
+                '日差しに気をつけて。',
+                'お散歩しよう！',
+            )
+        elif "雨" in weather:
+            forecast_tuple = (
+                '部屋の中で遊ぼう！',
+                '傘忘れていない？',
+            )
+        elif "雲" or "曇" or "くもり" in weather:
+            forecast_tuple = (
+                '晴れたらいいな',
+            )
+        elif "雪" in weather:
+            forecast_tuple = (
+                '雪合戦しよう！',
+                '寒さに気をつけて',
+            )
+        else:
+            forecast_tuple = (' ')
+        forecast_text += random.choice(forecast_tuple)
+
+        return forecast_text
+
     def send_mail(self):
         """
         Send mail with mailutils
@@ -55,18 +108,14 @@ class RobotLaunchEmail:
         message += "おはよう。"
         message += "{}時{}分です。\\n".format(
             current_time.hour, current_time.minute)
+        message += "{} \\n".format(self.get_weather_forecast(lang="ja"))
         message += "\\n"
 
         contents_title, contents, detail_url = self.get_tips()
         message += "今日の豆知識 \\n"
-        # Character code differences between python versions
-        # See https://stackoverflow.com/questions/54153986/handling-encode-when-converting-from-python2-to-python3 #NOQA
-        if sys.version_info.major == 2:
-            message += "{}: {} \\n".format(
-                contents_title.encode('utf-8'), contents.encode('utf-8'))
-        else:
-            message += "{}: {} \\n".format(contents_title, contents)
-        message += "詳細: {}".format(detail_url)
+        message += "{}: {} \\n".format(contents_title, contents)
+        message += "詳細: {} \\n".format(detail_url)
+        message += "\\n"
 
         # echo -e option is necessary in raspberry pi
         cmd = "echo -e '{}'".format(message)
