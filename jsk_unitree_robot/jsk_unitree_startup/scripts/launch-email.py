6984d627f3a616acefb0d4a6d42fff6bde465aba
Naoto Tsukamoto <naototukka0413@gmail.com>
2022-05-09 12:36:47 +0900

    [jsk_unitree_startup] Send Unitree-launch-email

---
+++ jsk_unitree_robot/jsk_unitree_startup/scripts/launch-email.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from bs4 import BeautifulSoup
+import datetime
+import rospy
+import subprocess
+import sys
+if sys.version_info.major == 2:
+    import urllib2
+else:
+    from urllib import request
+
+
+class RobotLaunchEmail:
+    """
+    Send email when robot launched with tips.
+    """
+    def get_tips(self):
+        """
+        Get tips from zatsuneta random article.
+
+        Returns:
+        ----------
+        title : str
+            article title
+        contents : str
+            article contents (heading)
+        detail_url :str
+            URL of the article written about the details
+        """
+        url = 'https://zatsuneta.com/category/random.html'
+        if sys.version_info.major == 2:
+            response = urllib2.urlopen(url)
+        else:
+            response = request.urlopen(url)
+        soup = BeautifulSoup(response, 'html5lib')
+        topstories = soup.find('div', class_="article")
+        title = topstories.find('a')['title']
+        detail_url = topstories.find('a')['href']
+        contents = topstories.find('p').text
+        response.close()
+
+        return title, contents, detail_url
+
+    def send_mail(self):
+        """
+        Send mail with mailutils
+        """
+        sender_address = "unitreepro@jsk.imi.i.u-tokyo.ac.jp"
+        receiver_address = "unitree@jsk.imi.i.u-tokyo.ac.jp"
+        current_time = datetime.datetime.now()
+        mail_title = "Unitree Go1 が起動しました"
+        message = ""
+        message += "おはよう。"
+        message += "{}時{}分です。\\n".format(
+            current_time.hour, current_time.minute)
+        message += "\\n"
+
+        contents_title, contents, detail_url = self.get_tips()
+        message += "今日の豆知識 \\n"
+        # Character code differences between python versions
+        # See https://stackoverflow.com/questions/54153986/handling-encode-when-converting-from-python2-to-python3 #NOQA
+        if sys.version_info.major == 2:
+            message += "{}: {} \\n".format(
+                contents_title.encode('utf-8'), contents.encode('utf-8'))
+        else:
+            message += "{}: {} \\n".format(contents_title, contents)
+        message += "詳細: {}".format(detail_url)
+
+        # echo -e option is necessary in raspberry pi
+        cmd = "echo -e '{}'".format(message)
+        cmd += " | /usr/bin/mail -s '{}' -r {} {}".format(
+            mail_title, sender_address, receiver_address)
+        exit_code = subprocess.call(cmd, shell=True)
+
+        rospy.loginfo('Title: {}'.format(mail_title))
+        if exit_code > 0:
+            rospy.logerr(
+                'Failed to send e-mail:  {} -> {}'.format(
+                    sender_address, receiver_address))
+            rospy.logerr("You may need to do '$ sudo apt install mailutils'")
+        else:
+            rospy.loginfo(
+                'Succeeded to send e-mail: {} -> {}'.format(
+                    sender_address, receiver_address))
+
+
+if __name__ == '__main__':
+    RobotLaunchEmail = RobotLaunchEmail()
+    RobotLaunchEmail.send_mail()
