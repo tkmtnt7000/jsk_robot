a70157227ab088101d55e9035f17fd36a2aea63f
Naoto Tsukamoto <naototukka0413@gmail.com>
2022-05-26 22:13:21 +0900

    update get-people-number

--- jsk_robot_common/jsk_robot_startup/scripts/get-people-number.py
+++ jsk_robot_common/jsk_robot_startup/scripts/get-people-number.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
+import math
 import rospy
 import subprocess
 
@@ -10,29 +11,40 @@ from jsk_recognition_msgs.msg import PeoplePoseArray
 class PeopleNumberNotifier():
     def __init__(self):
         rospy.init_node('get_people_number')
+        self.people_sum = 0
         self.people_number = 0
         self.last_publish_time = rospy.Time.now()
         self.last_subscribe_time = rospy.Time.now()
-        self.mail_duration = 60
+        self.last_calcurate_time = rospy.Time.now()
+        self.mail_duration = 180
         rospy.Subscriber("/people_pose", PeoplePoseArray, self.detect_cb)
+        # rospy.Subscriber("/edgetpu_human_pose_estimator/output/poses", PeoplePoseArray, self.detect_cb)
 
     def detect_cb(self, msg):
         if rospy.Time.now() - self.last_subscribe_time > rospy.Duration(1):
-            self.people_number += len(msg.poses)
+            self.people_sum += len(msg.poses)
             self.last_subscribe_time = rospy.Time.now()
             rospy.loginfo("length:{}".format(len(msg.poses)))
-            rospy.loginfo("sum:{}", self.people_number)
+            rospy.loginfo("sum:{}".format(self.people_sum))
+        if rospy.Time.now() - self.last_calcurate_time > rospy.Duration(5):
+            self.people_number += math.ceil(self.people_sum / 5.0)
+            self.people_sum = 0
+            self.last_calcurate_time = rospy.Time.now()
+            rospy.loginfo("number:{}".format(self.people_number))
 
     def send_mail(self):
         """
         Send mail with mailutils
         """
         sender_address = "unitreepro@jsk.imi.i.u-tokyo.ac.jp"
-        # receiver_address = "unitree@jsk.imi.i.u-tokyo.ac.jp"
-        receiver_address = "tsukamoto@jsk.imi.i.u-tokyo.ac.jp"
+        receiver_address = "unitree@jsk.imi.i.u-tokyo.ac.jp"
+        # receiver_address = "tsukamoto@jsk.imi.i.u-tokyo.ac.jp"
         mail_title = "お散歩中に人と出会いました．"
-        message = "{}人の人たちと出会ったよ．".format(
-            self.people_number / self.mail_duration)
+        if self.people_number == 0:
+            message = "誰とも出会わなかったよ．"
+        else:
+            message = "{}人の人たちと出会ったよ．".format(
+                int(math.ceil(self.people_number / (self.mail_duration / 5.0))))
         print(message)
         cmd = "echo -e '{}'".format(message)
         cmd += " | /usr/bin/mail -s '{}' -r {} {}".format(
@@ -46,14 +58,13 @@ class PeopleNumberNotifier():
         while not rospy.is_shutdown():
             rate.sleep()
             if rospy.Time.now() - self.last_publish_time > \
-               rospy.Duration(self.duration):
+               rospy.Duration(self.mail_duration):
                 try:
                     self.send_mail()
                 except Exception as e:
                     rospy.logerr("{}".format(e))
                     continue
-                rospy.loginfo('send mail.')
-                rospy.loginfo('waiting')
+                rospy.loginfo('send mail. Waiting...')
 
     # def run(self):
     #     seconds = 300
