d5bb366f1d81271e9a21ef861c6128a0d1c614c0
Naoto Tsukamoto <naototukka0413@gmail.com>
2022-05-26 17:10:32 +0900

    Update walk-mail.py!

--- jsk_robot_common/jsk_robot_startup/scripts/walk-mail.py
+++ jsk_robot_common/jsk_robot_startup/scripts/walk-mail.py
@@ -6,7 +6,12 @@ import time
 import re
 
 import requests
+import cv2
 # import nmcli
+
+import sensor_msgs.msg
+import rospy
+import cv_bridge
 
 
 cellNumberRe = re.compile(r"^Cell\s+(?P<cellnumber>.+)\s+-\s+Address:\s(?P<mac>.+)$")
@@ -65,19 +70,20 @@ def parse(content):
 
 
 
-def send_mail(place):
+def send_mail(place, attachment=None):
     """
     Send mail with mailutils
     """
     sender_address = "unitreepro@jsk.imi.i.u-tokyo.ac.jp"
     receiver_address = "unitree@jsk.imi.i.u-tokyo.ac.jp"
     # receiver_address = "yanokura@jsk.imi.i.u-tokyo.ac.jp"
-    mail_title = "お散歩中です。"
-    message = "お散歩してます。\\n今は{}を歩いているよ。".format(place)
-    print(message)
-    cmd = "echo -e '{}'".format(message)
-    cmd += " | /usr/bin/mail -s '{}' -r {} {}".format(
+    mail_title = u"スパーキー、お散歩中です。"
+    message = u"お散歩してます。\\n今は{}を歩いているよ。".format(place)
+    cmd = u"echo -e '{}'".format(message)
+    cmd += u" | /usr/bin/mail -s '{}' -r {} {}".format(
         mail_title, sender_address, receiver_address)
+    if attachment is not None:
+        cmd += ' -A {}'.format(attachment)
     exit_code = subprocess.call(cmd, shell=True)
 
 
@@ -85,13 +91,26 @@ class WalkNotifier(object):
 
     def __init__(self):
         # nmcli.disable_use_sudo()
-        pass
+        self.bridge = cv_bridge.CvBridge()
+        self.img = None
+        self.sub = rospy.Subscriber('/image_publisher/output',
+                                    sensor_msgs.msg.Image,
+                                    callback=self.callback,
+                                    queue_size=1)
+        rate = rospy.Rate(10)
+        self.cnt = 0
+        while not rospy.is_shutdown() and self.img is None:
+            rate.sleep()
+            rospy.loginfo('waiting image')
+
+    def callback(self, img_msg):
+        self.img = self.bridge.imgmsg_to_cv2(img_msg, desired_encoding='bgr8')
 
     def get_place(self):
         # aps = nmcli.device.wifi()
         # contents = [{"macAddress": ap.bssid, "age": 0} for ap in aps]
         aps = parse(scan('wlan1'))
-        contents = [{"macAddress": ap['mac'], "age": 0} for ap in aps]
+        contents = [{"macAddress": str(ap['mac']), "age": 0} for ap in aps]
         headers = {
             'Content-type': 'application/json',
         }
@@ -115,11 +134,20 @@ class WalkNotifier(object):
         address = response.json()
         a = address['results'][0]['formatted_address']
         print_address = " ".join(a.split(' ')[1:])
-        send_mail(print_address)
+
+        if self.img is not None:
+            img_path = '/tmp/image-{0:04}.jpg'.format(self.cnt)
+            cv2.imwrite(img_path, self.img)
+            self.cnt += 1
+            send_mail(print_address, img_path)
+        else:
+            send_mail(print_address)
 
     def run(self):
-        seconds = 300
-        while True:
+        seconds = 180
+        rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            rate.sleep()
             try:
                 notifier.get_place()
             except Exception as e:
@@ -132,5 +160,6 @@ class WalkNotifier(object):
 
 
 if __name__ == '__main__':
+    rospy.init_node('walk_mail')
     notifier = WalkNotifier()
     notifier.run()
