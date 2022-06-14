fdafdcbdfaac14804d8e79d364a35adc7feec83f
iory <ab.ioryz@gmail.com>
2022-06-15 02:58:13 +0900

    [jsk_unitree_robot/cross] Seperate docker build for Mac and Linux

--- jsk_unitree_robot/cross/build_ros1_dependencies.sh
+++ jsk_unitree_robot/cross/build_ros1_dependencies.sh
@@ -25,23 +25,48 @@ cp repos/ros1_dependencies.repos ${SOURCE_ROOT}/
 mkdir -p ${HOST_INSTALL_ROOT}/Python
 cp repos/go1_requirements.txt ${SOURCE_ROOT}/go1_requirements.txt
 
-docker run -it --rm \
-  -u $(id -u $USER) \
-  -e INSTALL_ROOT=${INSTALL_ROOT} \
-  -v ${PWD}/ros1_dependencies_build_scripts:/home/user/ros1_dependencies_build_scripts:ro \
-  -v ${PWD}/${SOURCE_ROOT}:/home/user/ros1_dependencies_sources:rw \
-  -v ${HOST_INSTALL_ROOT}/ros1_dependencies:/opt/jsk//${INSTALL_ROOT}/ros1_dependencies:rw \
-  -v ${HOST_INSTALL_ROOT}/Python:/opt/jsk/${INSTALL_ROOT}/Python:rw \
-  ros1-unitree:${TARGET_MACHINE} \
-  bash -c "\
-    set -xeuf -o pipefail && \
-    cd /home/user/ros1_dependencies_sources && \
-    vcs import --skip-existing --retry 3 --shallow src < ros1_dependencies.repos && \
-    for script_file in \$(ls /home/user/ros1_dependencies_build_scripts/|sort); do
-      /home/user/ros1_dependencies_build_scripts/\$script_file || exit 1;
-    done && \
-    pip install -U --user pip && \
-    export PYTHONPATH=\"/opt/jsk/System/ros1_dependencies/lib/python2.7/site-packages\" && \
-    export PKG_CONFIG_PATH=\"/opt/jsk/${INSTALL_ROOT}/ros1_dependencies/lib/pkgconfig\" && \
-    ~/.local/bin/pip install --prefix=/opt/jsk/${INSTALL_ROOT}/Python -r /home/user/ros1_dependencies_sources/go1_requirements.txt \
-    " 2>&1 | tee ${TARGET_MACHINE}_build_ros1_dependencies.log
+case ${OSTYPE} in
+    linux*)
+        docker run -it --rm \
+          -u $(id -u $USER) \
+          -e INSTALL_ROOT=${INSTALL_ROOT} \
+          -v ${PWD}/ros1_dependencies_build_scripts:/home/user/ros1_dependencies_build_scripts:ro \
+          -v ${PWD}/${SOURCE_ROOT}:/home/user/ros1_dependencies_sources:rw \
+          -v ${HOST_INSTALL_ROOT}/ros1_dependencies:/opt/jsk//${INSTALL_ROOT}/ros1_dependencies:rw \
+          -v ${HOST_INSTALL_ROOT}/Python:/opt/jsk/${INSTALL_ROOT}/Python:rw \
+          ros1-unitree:${TARGET_MACHINE} \
+          bash -c "\
+            set -xeuf -o pipefail && \
+            cd /home/user/ros1_dependencies_sources && \
+            vcs import --skip-existing --retry 3 --shallow src < ros1_dependencies.repos && \
+            for script_file in \$(ls /home/user/ros1_dependencies_build_scripts/|sort); do
+              /home/user/ros1_dependencies_build_scripts/\$script_file || exit 1;
+            done && \
+            pip install -U --user pip && \
+            export PYTHONPATH=\"/opt/jsk/System/ros1_dependencies/lib/python2.7/site-packages\" && \
+            export PKG_CONFIG_PATH=\"/opt/jsk/${INSTALL_ROOT}/ros1_dependencies/lib/pkgconfig\" && \
+            ~/.local/bin/pip install --prefix=/opt/jsk/${INSTALL_ROOT}/Python -r /home/user/ros1_dependencies_sources/go1_requirements.txt \
+            " 2>&1 | tee ${TARGET_MACHINE}_build_ros1_dependencies.log
+        ;;
+    darwin*)
+        docker run -it --rm \
+          -e INSTALL_ROOT=${INSTALL_ROOT} \
+          -v ${PWD}/ros1_dependencies_build_scripts:/home/user/ros1_dependencies_build_scripts:ro \
+          -v ${PWD}/${SOURCE_ROOT}:/home/user/ros1_dependencies_sources:rw \
+          -v ${HOST_INSTALL_ROOT}/ros1_dependencies:/opt/jsk//${INSTALL_ROOT}/ros1_dependencies:rw \
+          -v ${HOST_INSTALL_ROOT}/Python:/opt/jsk/${INSTALL_ROOT}/Python:rw \
+          ros1-unitree:${TARGET_MACHINE} \
+          bash -c "\
+            set -xeuf -o pipefail && \
+            pip install -U --user pip && \
+                export PYTHONPATH=\"/opt/jsk/System/ros1_dependencies/lib/python2.7/site-packages\" && \
+                export PKG_CONFIG_PATH=\"/opt/jsk/${INSTALL_ROOT}/ros1_dependencies/lib/pkgconfig\" && \
+                ~/.local/bin/pip install --prefix=/opt/jsk/${INSTALL_ROOT}/Python -r /home/user/ros1_dependencies_sources/go1_requirements.txt \
+            && \
+            cd /home/user/ros1_dependencies_sources && \
+            vcs import --skip-existing --retry 3 --shallow src < ros1_dependencies.repos && \
+            for script_file in \$(ls /home/user/ros1_dependencies_build_scripts/|sort); do
+              /home/user/ros1_dependencies_build_scripts/\$script_file || exit 1;
+            done" 2>&1 | tee ${TARGET_MACHINE}_build_ros1_dependencies.log
+        ;;
+esac
