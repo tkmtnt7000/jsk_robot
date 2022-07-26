81a9e908b7c344a9a6b98c98c365cca582f953e0
Naoto Tsukamoto <naototukka0413@gmail.com>
2022-07-26 09:37:32 +0900

    update

--- jsk_unitree_robot/jsk_unitree_startup/scripts/smach_to_mail.py
+++ jsk_unitree_robot/jsk_unitree_startup/scripts/smach_to_mail.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 
-import numpy as np
+import datetime
+# import numpy as np
 import rospy
 import pickle
 import base64
 import imghdr
-from io import StringIO
-from io import BytesIO
+# from io import StringIO
+# from io import BytesIO
 
+from jsk_robot_startup.msg import Email
+from jsk_robot_startup.msg import EmailBody
 from smach_msgs.msg import SmachContainerStatus
 
 import cv2
@@ -21,11 +24,11 @@ class SmachToMail():
         rospy.init_node('server_name')  # it should be 'smach_to_mail', but 'server_name' is the default name of smach_ros
         rospy.Subscriber(
             "~smach/container_status", SmachContainerStatus, self._status_cb)
-        # rospy.Subscriber(
-        #     "/usb_cam/image_raw", Image, self._img_cb)
         self.bridge = CvBridge()
-        self.dict_list = []
-
+        self.dict_list = [] # for status list
+        self.sender_address = "tsukamoto@jsk.imi.i.u-tokyo.ac.jp"
+        self.receiver_address = "tsukamoto@jsk.imi.i.u-tokyo.ac.jp"
+        
     # def _img_cb(self, msg):
     #     buff = StringIO()
     #     # buff = BytesIO()
@@ -56,7 +59,9 @@ class SmachToMail():
             # input_image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
             # cv_image = self.bridge.imgmsg_to_cv2(imgmsg, "bgr8")
             cv2.imshow("Image", cv_image)
-            file_path = "/tmp/{}.jpg".format(status_str)
+            dt_now = datetime.datetime.now()
+            file_path = "/tmp/{}_{}.jpg".format(
+                status_str.lower(), dt_now.strftime('%y%m%d%H%M%S'))
             rospy.loginfo("filepath:{}".format(file_path))
             if (next((x for x in self.dict_list if x["IMAGE"] == file_path), None)):
                 rospy.loginfo("same file name!!!!")
@@ -70,6 +75,41 @@ class SmachToMail():
         self.dict_list.append(tmp_dict)
         print(self.dict_list)
         print("info_str -> {}".format(info_str))
+
+        # if msg.active_states = "END":
+        #     self._send_mail()
+
+    def _send_mail():
+        email_msg = Email()
+        content1 = EmailBody()
+        content1.type = 'text'
+        content2 = EmailBody()
+        content2.type = 'text'
+        changeline = EmailBody()
+        changeline.type = 'html'
+        changeline.message = "<br>"
+        
+        content3 = EmailBody()
+        content3.type = 'img'
+        content3.file_path = "/tmp/hoge.jpg"
+        content3.img_size = 50
+        # print(msg.active_states)
+        # print(msg.local_data)
+        content1.message = msg.active_states[0]
+        rospy.loginfo("active state: {}".format(content1.message))
+        local_data = pickle.loads(msg.local_data)
+        content2.message = "".join(local_data)
+        # content2.message = msg.local_data
+        rospy.loginfo("local data: {}".format(content2.message))
+
+        email_msg.body = [content1, changeline, content2, changeline, content3]
+        email_msg.subject = 'Fetch Go to Kitchen Smach'
+
+        email_msg.sender_address = self.sender_address
+        email_msg.receiver_address = self.receiver_address
+
+        time.sleep(1)
+        self.pub.publish(email_msg)
 
 
 stm = SmachToMail()
