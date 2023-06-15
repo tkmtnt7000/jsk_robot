02b9281de7f6c20b6bf6477e464eaeb105e7f14c
Naoto Tsukamoto <naototukka0413@gmail.com>
2023-06-15 14:24:38 +0900

    [jsk_fetch_startup] Switch default update_workspace; not update forcefully

--- jsk_fetch_robot/jsk_fetch_startup/scripts/update_workspace.sh
+++ jsk_fetch_robot/jsk_fetch_startup/scripts/update_workspace.sh
@@ -5,5 +5,5 @@ cp $(rospack find jsk_robot_startup)/scripts/update_workspace_main.sh /tmp/updat
 # jsk_footstep_controller is not released for melodic
 # librealsense2 should not be installed from ROS repository
 # realsense-ros should not be installed from ROS repository
-/tmp/update_workspace.sh -r $(rospack find jsk_fetch_startup)/../jsk_fetch.rosinstall.$ROS_DISTRO -t fetch -s "jsk_footstep_controller jsk_footstep_planner librealsense2 realsense2_camera realsense2_description"
+/tmp/update_workspace.sh -n -r $(rospack find jsk_fetch_startup)/../jsk_fetch.rosinstall.$ROS_DISTRO -t fetch -s "jsk_footstep_controller jsk_footstep_planner librealsense2 realsense2_camera realsense2_description"
 rm /tmp/update_workspace.sh
